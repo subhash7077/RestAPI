c313f4348f2c4ce2a5603b17ddb736b180436f0a
Subhash <salke.subhash@gmail.com>
2025-07-16 14:04:01 +0530

    API

--- src/main/java/io/rest/test/BasicDemo.java
+++ src/main/java/io/rest/test/BasicDemo.java
@@ -1,31 +1,20 @@
 package io.rest.test;
 
+import io.jasonfiles.Payload;
 import io.restassured.RestAssured;
+import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.path.json.JsonPath.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class BasicDemo {
     public static void main(String[] args) {
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
-        given().log().all()
+            String response=RestAssured.given().log().all()
             .queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Patil house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\n").when().post("/maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+                .body(Payload.addPlace()).when().post("/maps/api/place/add/json")
+                .then().assertThat().statusCode(200).body("scope",equalTo("APP")).extract().asString();
+        System.out.println(response);
     }
 
 }
